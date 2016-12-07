b0f33e077f98567bdf84e3de19f4db5b9623d4da
ggiddy <nggitau@gmail.com>
2016-12-07 23:54:24 +0300

    (Feature) Saves and gets contacts

--- lib/app_functions.py
+++ lib/app_functions.py
@@ -1,9 +1,7 @@
 """Contains the main functions of the app"""
 import re
-from database.db_functions import DbManipulator
+from database import db_functions
 
-
-DBM = DbManipulator()
 def new_contact(line):
     """Validate and create a new contact"""
 
@@ -17,27 +15,28 @@ def new_contact(line):
         phone_number = line['<phone_number>']
 
         # Check if phone number has been saved
-        contact = DBM.get(first_name)
-        print contact
-        if phone_number == contact.phone_number:
-            # Phone number already saved.
-            return "That phone number already exists"
+        contact = db_functions.get(first_name)
+
+        if contact:
+            if phone_number == contact.phone_number:
+                # Phone number already saved.
+                return "That phone number already exists"
         else:
             # Save contact and return it
-            new = DBM.create(first_name, phone_number, last_name)
+            new = db_functions.create(first_name, phone_number, last_name)
             return new
-    else:
+    else: # Invalid phone
         return "Not a valid phone number"
-    # Query with validated phone if number exists, no. already added
 
-    # Else add user
     return "Creating the new contact %s %s %s" \
             % (line['<first_name>'], line['<last_name>'], line['<phone_number>'])
 
 def all_contacts():
     """Retrieve all contacts"""
-    contacts = DBM.get()
-    return contacts
+    contacts = db_functions.get()
+    if contacts:
+        print "Contacts found"
+        return contacts
 
 def search_contact():
     """Search a contact using the given parameters"""
